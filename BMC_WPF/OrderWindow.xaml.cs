e240b9b573826d91a4324793381e242383f0c5b5
reeshkeed <girish.daloso@gmail.com>
2017-03-15 09:25:58 -0800

    Initial commit

---
+++ BMC_WPF/OrderWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BMC_WPF
+{
+    /// <summary>
+    /// Interaction logic for OrderWindow.xaml
+    /// </summary>
+    public partial class OrderWindow
+    {
+        public OrderWindow()
+        {
+            InitializeComponent();
+
+            hideMember();
+        }
+
+        public void hideMember()
+        {
+            btnLogGuest.Visibility = Visibility.Hidden;
+            grbLogin.Visibility = Visibility.Hidden;
+
+            btnLogMember.Visibility = Visibility.Visible;
+            grbLoginGuest.Visibility = Visibility.Visible;
+        }
+
+        public void hideGuest()
+        {
+            btnLogMember.Visibility = Visibility.Hidden;
+            grbLoginGuest.Visibility = Visibility.Hidden;
+
+            btnLogGuest.Visibility = Visibility.Visible;
+            grbLogin.Visibility = Visibility.Visible;
+        }
+
+        private void btnLCont_Click(object sender, RoutedEventArgs e)
+        {
+            tbcOrderWindow.SelectedItem = tbItemShipping;
+        }
+
+        private void btnSCont_Click(object sender, RoutedEventArgs e)
+        {
+            tbcOrderWindow.SelectedItem = tbItemPayment;
+        }
+
+        private void btnLogMember_Click(object sender, RoutedEventArgs e)
+        {
+            hideGuest();
+        }
+
+        private void btnLogGuest_Click(object sender, RoutedEventArgs e)
+        {
+            hideMember();
+        }
+
+    }
+}
---
+++ BMC_WPF/Pages/MainWindowPages/LoginPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BMC_WPF.Pages.MainWindowPages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Mwin = new MainWindow();
+
+            if (txtUsername.Text.ToLower() == "o")
+            {
+                new OwnerWindow().Show();
+                Mwin.Close();
+            }
+            else if (txtUsername.Text.ToLower() == "c")
+            {
+                new CustomersWindow().Show();
+                Mwin.Close();
+            }else
+            {
+                new CustomersWindow().Show();
+                Mwin.Close();
+            }
+            
+        }
+
+        private void btnSignUpNow_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Pages/MainWindowPages/SignupPage.xaml", UriKind.RelativeOrAbsolute)); 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+    }
+}
